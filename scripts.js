b940b122aaf448063a83cc8f383298077eaf062d
Alejandro Yanez <70993283+since9teen94@users.noreply.github.com>
2022-07-04 10:09:03 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,19 @@
+function qs(selector) {
+    return document.querySelector(selector);
+}
+let button;
 async function getQuote() {
     let response = await fetch('https://www.officeapi.dev/api/quotes/random');
     let data = await response.json();
-    let res = {'quote':`"${data.data.content}"`, 'fullName':`- ${data.data.character.firstname} ${data.data.character.lastname}`}
-    return res;
+    return {'quote':`"${data.data.content}"`, 'author':`- ${data.data.character.firstname} ${data.data.character.lastname}`}
+}
+async function setNewQuote() {
+    const res = await getQuote();
+    qs('#text').innerText = res.quote;
+    qs('#author').innerText = res.author;
 }
 document.addEventListener('DOMContentLoaded', function () {
-    const quoteElement = document.getElementById('text')
-    const quoteAuthor = document.getElementById('author')
-    const button = document.getElementById('new-quote')
-
-    const setNewQuote = () => {
-            getQuote().then(res => {
-                quoteElement.innerText = res.quote
-                quoteAuthor.innerText = res.fullName
-            })
-        }
-
-    setNewQuote()
-    button.addEventListener('click', setNewQuote)
-    }, false);
+    button = qs('#new-quote');
+    button.addEventListener('click', setNewQuote);
+    await setNewQuote();
+});
